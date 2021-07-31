c6d3d39399c33ae9cf656919d22f88b97e2490ab
Fares A. Akel C <fares@startblock.tech>
2021-07-31 09:54:07 -0500

    Add cli

---
+++ cli.js
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+/* eslint-disable no-unused-expressions */
+const yargs = require('yargs/yargs');
+const { hideBin } = require('yargs/helpers');
+const { parse, stringify } = require('.');
+
+yargs(hideBin(process.argv))
+  .command('parse [roman]', 'parse roman numeral to number', (yrg) => yrg
+    .positional('roman', {
+      describe: 'roman numeral',
+    }), (argv) => {
+    try {
+      console.log(parse(argv.roman));
+    } catch (error) {
+      console.log(error.message);
+    }
+  })
+  .command('stringify [number]', 'transform number to roman numeral', (yrg) => yrg
+    .positional('number', {
+      describe: 'number to transform',
+    }), (argv) => {
+    try {
+      console.log(stringify(argv.number));
+    } catch (error) {
+      console.log(error.message);
+    }
+  })
+  .argv;
